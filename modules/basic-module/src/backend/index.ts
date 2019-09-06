92391f1e6671ed07990c6d037cf97ae80848d0aa
Simon-Pierre <spg@MacBook-Air-de-Valerie.local>
2019-09-06 15:03:07 -0400

    Added empty bot template

--- modules/basic-module/src/backend/index.ts
+++ modules/basic-module/src/backend/index.ts
@@ -3,9 +3,14 @@ import * as sdk from 'botpress/sdk'
 const onServerStarted = async (bp: typeof sdk) => {}
 const onServerReady = async (bp: typeof sdk) => {}
 
+const botTemplates: sdk.BotTemplate[] = [
+  { id: 'empty-bot', name: 'Empty Bot', desc: `Start fresh with a clean flow` }
+]
+
 const entryPoint: sdk.ModuleEntryPoint = {
   onServerStarted,
   onServerReady,
+  botTemplates,
   definition: {
     name: 'basic-module',
     menuIcon: 'none',
