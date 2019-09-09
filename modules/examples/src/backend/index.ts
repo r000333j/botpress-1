64be4704286d6f83fcfc1b22e4e98d17c882a07d
Simon-Pierre <spg@MacBook-Air-de-Valerie.local>
2019-09-09 08:08:20 -0400

    Weather bot name

--- modules/examples/src/backend/index.ts
+++ modules/examples/src/backend/index.ts
@@ -4,7 +4,7 @@ const onServerStarted = async (bp: typeof sdk) => {}
 const onServerReady = async (bp: typeof sdk) => {}
 
 const botTemplates: sdk.BotTemplate[] = [
-  { id: 'weather-bot', name: 'Weather Bot', desc: `Gets the weather for a city` }
+  { id: 'wbot', name: 'Weather Bot', desc: 'Gets the weather for a city' },
 ]
 
 const entryPoint: sdk.ModuleEntryPoint = {
